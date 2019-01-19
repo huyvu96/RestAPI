c4c101a6631a9e54b817661554ea2711f09bcc46
vunahnexlesoft <vunah@nexlesoft.com>
2019-01-19 22:13:11 +0700

    update

---
+++ Models/Channel.js
@@ -0,0 +1,30 @@
+var db = require('../ConnectDatabase/Dbconnection');
+
+var Channel = {
+    getAllChannel: function () {
+        var sql = "select * from channel";
+        return new Promise(function (resolve, reject) {
+            db.query(sql, function (err, result) {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(result)
+                }
+            })
+        });
+    },
+    getCalenderByChannel: function () {
+        var sql = "SELECT movies.id, movies.title, movies.title_en, movies.overview, movies.poster_path, movies.backdrop_path, movies.rating, time_of_date.time as start_time" +
+            "from movies inner join time_of_date on movies.id = time_of_date.id_movie;";
+        return new Promise(function (resolve, reject) {
+            db.query(sql, function (err, result) {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(result)
+                }
+            })
+        });
+    },
+};
+module.exports = Channel;
