9c04af6292a1338512f71e8565196f74de9d6318
vunahnexlesoft <vunah@nexlesoft.com>
2019-01-26 21:42:55 +0700

    asd

--- Routers/routerMovies.js
+++ Routers/routerMovies.js
@@ -395,7 +395,7 @@ router.get('/watchlist', global.verifyToken, async function (req, res, next) {
     });
 
 });
-router.get('/start-stream?', async function (req, res, next) {
+router.post('/start-stream?', async function (req, res, next) {
     let {movies} = req.body;
     let path = "https://firebasestorage.googleapis.com/v0/b/livestreaming-46229.appspot.com/o/guardians2.mp4?alt=media&token=eb9467c6-6f16-475c-b541-14342103dce7";
     let path2 ="https://firebasestorage.googleapis.com/v0/b/livestreaming-46229.appspot.com/o/sample.mp4?alt=media&token=48fb2aa6-61d8-4848-8eaa-c60813b04df2";
