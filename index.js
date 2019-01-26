e022d9270375e7265d93c9ccdc75f14410031bc2
vunahnexlesoft <vunah@nexlesoft.com>
2019-01-27 00:20:33 +0700

    asd

--- index.js
+++ index.js
@@ -44,22 +44,24 @@ const jobBackground = new cron.CronJob('0 */10 * * * * *', async function () { /
         }
     }
     else if (movies.length === 1) {
-        let currentTime = await global.convertTimeToSecond(global.getDateTime());
-        let timeOfMoviewStreaming = await global.convertTimeToSecond(movies[0].time);
-        console.log('Has one item', currentTime, timeOfMoviewStreaming);
-        if (currentTime > timeOfMoviewStreaming) {//Xet thoi gian hien tai > time phan tu thu [0] hay khong
-            let episodes = await Episodes.getAllEpisodesbyIdMovie(movies[0].id_movie);
-            let url_link = await episodes[0].url_link;//Lay URL phim ke tiep
-            cmd.get(`killall ffmpeg`);//Huy lenh
-            let timeOutStream = setTimeout(() =>{
-                timeOutStream = null;
-                clearTimeout(timeOutStream);
-                console.log('start streaming');
-                global.startStreamming(url_link);//Streaming phim tiep theo
-            },5000);
-            console.log('delete movie:', movies[0].id_movie);
-            Channel.deleteMovie(movies[0].id_movie);//Delete phim truoc do
-        }
+        console.log('delete movie:', movies[0].id_movie);
+        Channel.deleteMovie(movies[0].id_movie);//Delete phim truoc do
+        // let currentTime = await global.convertTimeToSecond(global.getDateTime());
+        // let timeOfMoviewStreaming = await global.convertTimeToSecond(movies[0].time);
+        // console.log('Has one item', currentTime, timeOfMoviewStreaming);
+        // if (currentTime > timeOfMoviewStreaming) {//Xet thoi gian hien tai > time phan tu thu [0] hay khong
+        //     let episodes = await Episodes.getAllEpisodesbyIdMovie(movies[0].id_movie);
+        //     let url_link = await episodes[0].url_link;//Lay URL phim ke tiep
+        //     console.log('delete movie:', movies[0].id_movie);
+        //     Channel.deleteMovie(movies[0].id_movie);//Delete phim truoc do
+        //     cmd.get(`killall ffmpeg`);//Huy lenh
+        //     let timeOutStream = setTimeout(() =>{
+        //         timeOutStream = null;
+        //         clearTimeout(timeOutStream);
+        //         console.log('start streaming');
+        //         global.startStreamming(url_link);//Streaming phim tiep theo
+        //     },5000);
+        // }
     }
 });
 jobBackground.start();
