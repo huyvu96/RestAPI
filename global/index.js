22d586b5e419b825664f110867d284f18e4b2bf1
vunahnexlesoft <vunah@nexlesoft.com>
2019-01-26 22:13:09 +0700

    asd

--- global/index.js
+++ global/index.js
@@ -25,10 +25,10 @@ const global = {
     },
     toHHMMSS: function(secs){
         var sec_num = parseInt(secs, 10);
-        var hours   = Math.floor(sec_num / 3600) % 24
-        var minutes = Math.floor(sec_num / 60) % 60
-        var seconds = sec_num % 60
-        return [,minutes,seconds]
+        var hours   = Math.floor(sec_num / 3600) % 24;
+        var minutes = Math.floor(sec_num / 60) % 60;
+        var seconds = sec_num % 60;
+        return [hours,minutes,seconds]
             .map(v => v < 10 ? "0" + v : v)
             //.filter((v,i) => v !== "00" || i > 0)
             .join(":")
