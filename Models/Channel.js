2a33c4d44dc1d8803b397e738807fbbee3c1b936
vunahnexlesoft <vunah@nexlesoft.com>
2019-01-27 23:28:52 +0700

    asd

--- Models/Channel.js
+++ Models/Channel.js
@@ -14,7 +14,7 @@ var Channel = {
         });
     },
     getCalenderByChannel: function () {
-        var sql = "SELECT movies.id, movies.title, movies.title_en, movies.overview, movies.poster_path, movies.backdrop_path, movies.rating, time_of_date.time as start_time from movies inner join time_of_date on movies.id = time_of_date.id_movie";
+        var sql = "SELECT movies.id, movies.title, movies.title_en, movies.overview, movies.poster_path, movies.backdrop_path, movies.rating, time_of_date.time as start_time, time_of_date.seconds as duration from movies inner join time_of_date on movies.id = time_of_date.id_movie";
         return new Promise(function (resolve, reject) {
             db.query(sql, function (err, result) {
                 if (err) {
